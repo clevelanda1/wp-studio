362b18eb81606712b59866c3ac2ae1993c830b71
clevelanda1 <clevelandaus1@gmail.com>
2025-08-22 21:53:40 -0500

    25

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -168,10 +168,10 @@ const Sidebar: React.FC = () => {
               >
                 <LogOut className="w-5 h-5" />
                 
-                {/* Tooltip for collapsed state */}
+                {/* Tooltip for collapsed state 
                 <div className="hidden lg:block lg:group/logout-hover:block absolute left-full ml-2 px-2 py-1 bg-zinc-800 text-white text-xs rounded-md whitespace-nowrap z-50 pointer-events-none">
                   Sign Out1
-                </div>
+                </div>*/}
               </button>
             </div>
           </div>
